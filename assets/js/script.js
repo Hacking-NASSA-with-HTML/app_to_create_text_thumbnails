fc58b4ac3e9cb115a2fc3fbebbb8e4b39e09e2a6
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-22 00:54:53 +0400

    refactor: brush up the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,6 @@ const generatedContent = document.getElementById('body')
 
 generatedContent.innerHTML = mainPageContent()
 
-// function mainPageContent() {
-//     const renderMainPageContent =
-//         `<main id="main">
-//             <h1 class="titleH1">Array iteration cheat sheet</h1>
-//             <p class="paragraph">
-//                 <a class="link" title="forEach Method" href="./forEach/forEach.html">forEach Method</a>
-//             </p>
-//             <p class="paragraph">
-//                 <a class="link" title="Map Method" href="./map/map.html">map Method</a>
-//             </p>
-//     </main>`
-//     return renderMainPageContent
-// }
 
 function mainPageContent() {
     const renderMainPageContent =
