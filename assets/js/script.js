a611b6a66930d12deba8da0e1aeaf5bd83529eb8
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-22 01:14:57 +0400

    feat: add tips for OG image sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,20 @@ function mainPageContent() {
             <h1 class="titleH1">Create text thumbnails</h1>
             <div class="wrapper">
                 <div class="canvas_1200_630">
-                    <p class="thumb-text_1200_630">Lorem Ipsum</p>
-                    <p class="thumb-text_1200_630">Lorem Ipsum</p>
-                    <p class="thumb-text_1200_630">Lorem Ipsum</p>
+                    <p class="thumb-text_1200_630">What size should open graph image be?</p>
+                    <p class="thumb-text_1200_630">1200 x 630 pixels for Facebook Open Graph.</p>
+                    <p class="thumb-text_1200_630">This Canvas already in 1200x630</p>
+                    <p class="thumb-text_1200_630">Customize your text, outline color in CSS file.</p>
+                    <p class="thumb-text_1200_630">Make a screenshot.</p>
                 </div>
             </div>
             <div class="wrapper">
                 <div class="canvas_400_400">
-                    <p class="thumb-text_400_400">Lorem Ipsum</p>
-                    <p class="thumb-text_400_400">Lorem Ipsum</p>
-                    <p class="thumb-text_400_400">Lorem Ipsum</p>
+                    <p class="thumb-text_400_400">What size should open graph image be?</p>
+                    <p class="thumb-text_400_400">around 400x400 pixels for WhatsApp.</p>
+                    <p class="thumb-text_400_400">This Canvas already in 400x400</p>
+                    <p class="thumb-text_400_400">Customize your text, outline color in CSS file.</p>
+                    <p class="thumb-text_400_400">Make a screenshot.</p>
                 </div>
             </div>
         </main>`
