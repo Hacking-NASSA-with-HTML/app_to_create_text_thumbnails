543966bdd61eb254652bb5f1561ce18b69027ec6
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-22 02:04:39 +0400

    feat: add improved user tips

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,14 @@ function mainPageContent() {
                     <p class="thumb-text_1200_630">What size should open graph image be?</p>
                     <p class="thumb-text_1200_630">1200 x 630 pixels for Facebook Open Graph.</p>
                     <p class="thumb-text_1200_630">This Canvas already in 1200x630</p>
-                    <p class="thumb-text_1200_630">Customize your text, outline color in CSS file.</p>
+                    <p class="thumb-text_1200_630">Customize your text in JavaScript file, outline color in CSS file.</p>
                     <p class="thumb-text_1200_630">Make a screenshot.</p>
                 </div>
                 <div class="canvas_400_400">
                     <p class="thumb-text_400_400">What size should open graph image be?</p>
                     <p class="thumb-text_400_400">around 400x400 pixels for WhatsApp.</p>
                     <p class="thumb-text_400_400">This Canvas already in 400x400</p>
-                    <p class="thumb-text_400_400">Customize your text, outline color in CSS file.</p>
+                    <p class="thumb-text_400_400">Customize your text in JavaScript file, outline color in CSS file.</p>
                     <p class="thumb-text_400_400">Make a screenshot.</p>
                 </div>
             </div>
